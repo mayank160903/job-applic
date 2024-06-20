774f60de047a70123873e6b565ffca47ab8aea00
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-06-20 21:50:32 +0530

    pushing

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Content = () => {
   return (
-    <div className='p-20 text-3xl text-center'>
+    <div className='px-20 py-10 text-3xl text-center'>
       Looking for a MERN Stack Web Developer sound in constructing web applications in quick time. Register below.
       <b>Fill all the details carefully.</b>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='text-center p-6 bg-black text-4xl font-sans font-bold text-white'>
-      Level-2 : Event Registration Form
+      Level-2 : Job Application Form
     </div>
   )
 }
